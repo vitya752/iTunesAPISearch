042077aad3f61eacb5274577ad3c96d4c37ffc28
vitya752 <vitya752@ukr.net>
2019-09-26 00:11:43 +0300

    Updates

---
+++ precache-manifest.ca2d13001474fe3c86e51c1f921c33e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b2069f2426f3a4441a47a144d4467f",
+    "url": "/iTunesAPISearch/index.html"
+  },
+  {
+    "revision": "8c707b645db5c7c1b431",
+    "url": "/iTunesAPISearch/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c793f75c0b4bf53ff7d8",
+    "url": "/iTunesAPISearch/static/css/main.df9e1555.chunk.css"
+  },
+  {
+    "revision": "8c707b645db5c7c1b431",
+    "url": "/iTunesAPISearch/static/js/2.330a9441.chunk.js"
+  },
+  {
+    "revision": "c793f75c0b4bf53ff7d8",
+    "url": "/iTunesAPISearch/static/js/main.e72a4094.chunk.js"
+  },
+  {
+    "revision": "2bf864858fe43d740170",
+    "url": "/iTunesAPISearch/static/js/runtime-main.96a3b351.js"
+  }
+]);
